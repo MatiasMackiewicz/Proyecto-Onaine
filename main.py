1c2409c2c96dbfed81f366937e362115d4915abe
Matias Mackiewicz <matiadri2005@hotmail.com>
2020-10-26 19:31:54 -0300

    practicas basicas
    sarasa

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
 
 
-def print_hi(name):
-    print(f'Hi, {name}')
+L1 = ["La Plata"]
+L2 = ["Mar del Plata"]
+Cities = []
+Cities.append(L1)
+Cities.append(L2)
+print(Cities)
+
+def definir(Cities):
+ """#define nada"""
 
 
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+os.system("pause")
